c462d9583fe0a2614fea151126f9d0df6105cd83
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-04-12 20:30:45 +0200

    Add files via upload

--- Info 7/Main.cpp
+++ Info 7/Main.cpp
@@ -13,11 +13,7 @@ int main(int argc, char* argv[])
     board.move_piece(1, 0, 3, 0, plateau);
     board.write_fen(plateau, "fen.txt");
     board.empty_board(plateau);
-    board.print_board(plateau);
     board.read_fen(plateau, "fen.txt");
-    board.print_board(plateau);
-
-    // Test
 
     system("pause");
     return 0;
--- Info 7/board.cpp
+++ Info 7/board.cpp
@@ -2,16 +2,12 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include "type.h"
 using namespace std;
 
-#pragma region Methods
-
 // Print FEN
 void board::print_fen(type::type_board type_board) {cout << "FEN: " << type_board.get_fen() << endl;}
-
-// Print the type_board
-
 
 // Empty the type_board
 void board::empty_board(type::type_board type_board) {type_board.fen_board = "8/8/8/8/8/8/8/8";}
@@ -22,4 +18,26 @@ void board::reset_game(type::type_board type_board) {type_board.fen_board = "rnb
 // Move a piece
 void board::move_piece(const int i1, const int j1, const int i2, const int j2, type::type_board type_board) {char piece = type_board.get_piece(i1, j1); type_board.set_piece(i1, j1, ' '); type_board.set_piece(i2, j2, piece);}
 
-#pragma endregion Methods
+// Write FEN in a file
+void board::write_fen(type::type_board type_board, std::string file_name)
+{
+    ofstream file;
+    file.open(file_name);
+    if (file.is_open()) {file << type_board.get_fen(); file.close();}
+    else cout << "Unable to open file";
+}
+
+// Read FEN in a file
+void board::read_fen(type::type_board type_board, std::string file_name)
+{
+    ifstream file;
+    file.open(file_name);
+    if (file.is_open())
+    {
+        string fen;
+        getline(file, fen);
+        type_board.set_fen(fen);
+        file.close();
+    }
+    else cout << "Unable to open file";
+}
