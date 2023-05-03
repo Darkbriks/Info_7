0f92cfbebabb944283f0cd9cbdae256ba3faa13f
Zyre28 <116887859+Zyre28@users.noreply.github.com>
2023-05-03 11:57:54 +0200

    Update view.h

--- Info 7/view.h
+++ Info 7/view.h
@@ -1,8 +1,12 @@
-﻿#ifndef VIEW
+#ifndef VIEW
 #define VIEW
 #include "type.h"
+#include <string>
 
-void print_square(type_board B,int x,int y);
+
+void print_square_color(char piece, int x,int y);
 void print_board(type_board B);
+void set_background(int x,int y);
+void set_foreground(char piece);
 
 #endif
