f7de616270012e523a80fc3db660feb5f8e8514d
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-04 16:01:36 +0200

    Add mask of rook and king

--- Info 7/Main.cpp
+++ Info 7/Main.cpp
@@ -9,44 +9,15 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    // Test board functions
-    type_board type_board;
-    type_mask mask = empty_mask();
-    clear_mask(mask);
-    for(int i=7;i>=0;i--)
-    {
-        mask.set_mask(i,7,1);
-        mask.set_mask(i,6,2);
-        mask.set_mask(i,5,3);
-        mask.set_mask(i,4,4);
-        mask.set_mask(i,3,5);
-        mask.set_mask(i,2,6);
-        mask.set_mask(i,1,7);
-    }
-    type_board.set_board_with_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    print_board(type_board, mask);
+    type_board  board;
+    board.set_board_with_fen("rnbqkbnr/pppppppp/8/8/3R3P/8/PPPpPpPP/4KpNR w KQkq - 0 1");
     
-    system("pause");
-    move_piece(1, 5, 3, 5, type_board);
-    print_board(type_board, mask);
-    print_fen(type_board);
-    cout << endl;
-    system("pause");
-    move_piece(6, 5, 4, 5, type_board);
-    print_board(type_board, mask);
-    print_fen(type_board);
-    cout << endl;
-
-
-    /*char16_t c1{0x2654};
-    wcout << c1 << endl;
-    cout << c1 << endl;
-
-    cout << "\0xE2 \0x99 \0x9E" << endl;
-    cout << "\0x265E" << endl;
-    cout << "\0x0000265E" << endl;
-    cout << '\u265E' << endl;*/
+    type_mask mask;
+    //highlight_possible_moves(0, 3, &mask, board);
+    highlight_possible_moves_rook(3, 4, &mask, board);
     
+    print_board(board, mask);
+
     system("pause");
     
     return 0;
--- Info 7/mask.cpp
+++ Info 7/mask.cpp
@@ -1,4 +1,5 @@
 ﻿#include "mask.h"
+#include "board.h"
 #include <iostream>
 
 using namespace std;
@@ -9,83 +10,44 @@ int get_mask(const type_mask mask) {return *mask.mask;}
 
 void clear_mask(const type_mask mask) {for (int i = 0; i < 64; i++) mask.set_mask(i/8, i%8, 0);}
 
-void highlight_possible_moves(int x, int y, type_mask mask, type_board board)
+void highlight_possible_moves(int x, int y, type_mask *mask, type_board board)
 {
     if (board.get_piece(x, y) == 'K' or board.get_piece(x, y) == 'k') {highlight_possible_moves_king(x, y, mask, board);}
     if (board.get_piece(x, y) == 'R' or board.get_piece(x, y) == 'r') {highlight_possible_moves_rook(x, y, mask, board);}
 }
 
-void highlight_possible_moves_king(int x, int y, type_mask mask, type_board board) {}
+void highlight_possible_moves_king(int x, int y, type_mask *mask, type_board board)
+{
+    for (int i = 1; i > -2; i--)
+    {
+        for (int j = -1; j < 2; j++)
+        {
+            if (i == 0 and j == 0) {mask->set_mask(x, y, 5);} // Check if the position is the king
+            else if (board.get_piece(x+j, y+i) == ' ') {mask->set_mask(x+j, y+i, 4);}
+            else if (is_enemy(board.get_piece(x, y), board.get_piece(x+j, y+i)) == true) {mask->set_mask(x+j, y+i, 1);}
+        }
+    }
+}
 
 void highlight_possible_moves_rook(int x, int y, type_mask* mask, type_board board)
 {
-    mask->set_mask(x,y,5);
-    int i =x+1;
-    while(i<=7)
+    for (int i = 0; i < 4; i++)
     {
-        if(board.get_piece(i,y) != ' ')
+        int n = 0, m = 0;
+        while (true)
         {
-            if(is_enemy(board.get_piece(x,y),board.get_piece(i,y)))
+            switch (i)
             {
-                mask->set_mask(i,y,1);
+                case 0: n++; break;
+                case 1: n--; break;
+                case 2: m++; break;
+                case 3: m--; break;
+                default: break;
             }
-            i = 8;
-        }
-        else
-        {
-            mask->set_mask(i,y,4);
-            i=i+1;
+            if (board.get_piece(x+n, y+m) == ' ') mask->set_mask(x+n, y+m, 4);
+            else if (is_enemy(board.get_piece(x, y), board.get_piece(x+n, y+m)) == true) {mask->set_mask(x+n, y+m, 1); break;}
+            else break;
         }
     }
-    i =y-1;
-    while(i>=0)
-    {
-        if(board.get_piece(x,i) != ' ')
-        {
-            if(is_enemy(board.get_piece(x,y),board.get_piece(x,i)))
-            {
-                mask->set_mask(x,i,1);
-            }
-            i=i-1;
-        }
-        else
-        {
-            mask->set_mask(x,i,4);
-            i=i-1;
-        }
-    }
-    i =x-1;
-    while(i>=0)
-    {
-        if(board.get_piece(i,y) != ' ')
-        {
-            if(is_enemy(board.get_piece(x,y),board.get_piece(i,y)))
-            {
-                mask->set_mask(i,y,1);
-            }
-            i=-1;
-        }
-        else
-        {
-            mask->set_mask(i,y,4);
-            i=i-1;
-        }
-    }
-    i =y+1;
-    while(i<=7)
-    {
-        if(board.get_piece(x,i) != ' ')
-        {
-            if(is_enemy(board.get_piece(x,y),board.get_piece(x,i)))
-            {
-                mask->set_mask(x,i,1);
-            }
-            i = 8;
-        }
-        else
-        {
-            mask->set_mask(x,i,4);
-            i=i+1;
-        }
-    }
+    mask->set_mask(x, y, 5);
 }
