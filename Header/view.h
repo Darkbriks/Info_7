7654208697aebf71fdac0faeb236f59e9e29a667
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-07 10:05:17 +0200

    Move and fix view.h and view.cpp
    Fix i and j (x and y) with the same convention as type.h

---
+++ Header/view.h
@@ -0,0 +1,13 @@
+﻿#ifndef VIEW
+#define VIEW
+
+#include "type.h"
+
+// Functions
+void print_square_color(char piece, int color);
+void print_board(type_board board);
+void print_board(type_board board, type_mask mask);
+void set_background(int color);
+void set_foreground(char piece);
+
+#endif
