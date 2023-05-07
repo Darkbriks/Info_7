b3f8bf4b53d831760d06c8d7747f4c7598627ef7
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-07 10:40:47 +0200

    Move and fix mask.h

---
+++ Info 7/Header/mask.h
@@ -0,0 +1,16 @@
+﻿#ifndef MASK
+#define MASK
+
+#include "type.h"
+
+// Methods
+type_mask empty_mask();
+int get_mask(type_mask mask);
+void clear_mask(type_mask mask);
+
+// Higlihts functions
+void highlight_possible_moves(int x, int y, type_mask *mask, type_board board);
+void highlight_possible_moves_king(int x, int y, type_mask *mask, type_board board);
+void highlight_possible_moves_rook(int x, int y, type_mask *mask, type_board board);
+
+#endif
