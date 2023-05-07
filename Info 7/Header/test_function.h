6d50388f75fc942cece5d18e54ead0c9e33118f7
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-07 10:03:02 +0200

    Add test_function
    test_function is there to not clutter Main.cpp

---
+++ Info 7/Header/test_function.h
@@ -0,0 +1,12 @@
+﻿#ifndef TEST_FUNCTION
+#define TEST_FUNCTION
+
+#include "type.h"
+
+void test_all_type_board_getters(type_board board);
+void test_all_type_board_setters(type_board board);
+
+void test_all_type_mask_getters(type_mask mask);
+void test_all_type_mask_setters(type_mask mask);
+
+#endif
