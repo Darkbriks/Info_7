8e29d846474da4af92860227d5d07f5f6f13021d
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-08 10:44:15 +0200

    Add documentation for mask.h

--- Info 7/Header/mask.h
+++ Info 7/Header/mask.h
@@ -4,13 +4,16 @@
 #include "type.h"
 
 // Methods
-type_mask empty_mask();
-int get_mask(type_mask mask);
-void clear_mask(type_mask mask);
+type_mask empty_mask(); // Returns an empty mask
+void clear_mask(type_mask mask); // Resets the mask to 0
 
 // Higlihts functions
-void highlight_possible_moves(int x, int y, type_mask *mask, type_board board);
-void highlight_possible_moves_king(int x, int y, type_mask *mask, type_board board);
-void highlight_possible_moves_rook(int x, int y, type_mask *mask, type_board board);
+void highlight_possible_moves(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a piece
+void highlight_possible_moves_king(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a king (one square in all directions)
+void highlight_possible_moves_rook(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a rook (all squares in horizontal and vertical directions)
+void highlight_possible_moves_bishop(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a bishop (all squares in diagonal directions)
+void highlight_possible_moves_queen(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a queen (all squares in all directions)
+void highlight_possible_moves_knight(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a knight (L shape)
+void highlight_possible_moves_pawn(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a pawn (one square forward, two squares forward (if it's the first move), and one square diagonally forward (if there's an enemy piece))
 
 #endif
