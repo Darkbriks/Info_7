e0809260a9191ad6c7e0bdef0f8cddf12fb2acaa
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-08 10:27:50 +0200

    Add documentation for board.h

--- Info 7/Header/board.h
+++ Info 7/Header/board.h
@@ -4,14 +4,14 @@
 #include "type.h"
 
 // Methods
-void print_fen(type_board type_board);
-void empty_board(type_board type_board);
-void reset_board(type_board type_board);
-void move_piece(int i1, int j1, int i2, int j2, type_board type_board);
+void print_fen(type_board type_board); // Print the FEN string corresponding to the type_board
+void empty_board(type_board type_board); // Empty the type_board
+void reset_board(type_board type_board); // Reset the game
+void move_piece(int i1, int j1, int i2, int j2, type_board type_board); // Move a piece
 bool is_enemy(char piece1, char piece2); // Check if two pieces are enemies
 
 // Write and read FEN in a file
-void write_fen(type_board type_board, std::string file_name);
-void read_fen(type_board type_board, std::string file_name);
+void write_fen(type_board type_board, std::string file_name); // Write FEN corresponding to the type_board in a file
+void read_fen(type_board type_board, std::string file_name); // Read FEN in a file and set the type_board with it
 
 #endif
