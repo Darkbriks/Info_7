c918c7f4cd783c0e7e480712effd5047d8f1d21b
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-05-10 16:00:58 +0200

    Fix sous_mask_choices

--- Info 7/Source/mask.cpp
+++ Info 7/Source/mask.cpp
@@ -198,10 +198,7 @@ int highlight_possible_moves_pawn(const int x, const int y, type_mask* mask, typ
 {
     mask->set_mask(x, y, 5); int n = 0;
     int j = 1, k = 1;
-    if(board.get_piece(x, y) != 'P')
-    {
-        j = -1; k = 6;
-    }
+    if(board.get_piece(x, y) != 'P') {j = -1; k = 6;}
     if (board.get_piece(x, y + j) == ' ')
     {
         mask->set_mask(x, y + j, 4); n++;
@@ -263,7 +260,7 @@ bool attacked_piece(const char piece, const int x, const int y, type_board board
 }
 
 /**
- * \brief Highlight all pieces that can be attacked
+ * \brief Highlight all pieces that can be attacked by the color
  * \param color the color of the player
  * \param mask the mask to highlight
  * \param board the board
@@ -307,13 +304,12 @@ void mask_choices_menu(type_board board, type_mask mask)
     int choix = 0;
     choix = sous_mask_choices(choix);
     sous_mask_choices(choix,  board, &mask);
-
 }
 
 int sous_mask_choices(int choix)
 {
-    cout << "Voici les differents affichages possibles :" << endl << "    1.Les pieces qui peuvent se deplacer" << endl << "    2.Les pieces ennemies pouvant etre prises" << endl << "    3.Les pieces ennemies pouvant mettre en danger l'une des votres"
-    << endl << "    4.Les actions possibles d'une piece specifique" << endl << "Votre choix : ";
+    cout << "Voici les differents affichages possibles :" << endl << "\t1.Les pieces qui peuvent se deplacer" << endl << "\t2.Les pieces ennemies pouvant etre prises" << endl << "\t3.Les pieces ennemies pouvant attaquer une piece specifique"
+    << endl << "\t4.Les actions possibles d'une piece specifique" << endl << "Votre choix : ";
     cin >> choix;
     while (choix < 0 or choix > 4)
     {
@@ -325,12 +321,16 @@ int sous_mask_choices(int choix)
 
 void sous_mask_choices(int choix, type_board board, type_mask* mask)
 {
-    int  x, y;
+    int x, y;
     switch (choix)
     {
-    case 1: ; break; 
-    case 2: ; break; 
-    case 3: ; break; 
+    case 1: highlight_movable_pieces(board.get_turn(), mask, board); print_board(board, *mask); break;
+    case 2: highlight_attacked_pieces(board.get_turn(), mask, board); print_board(board, *mask); break; 
+    case 3:
+        {
+            while (true) {cout << "Colone et ligne de la piece : "; cin >> x >> y; if (board.get_piece(x-1,y-1) != ' ') {break;} cout << "La case est vide, reessayez" << endl;}
+            highlight_take_pieces(x-1, y-1, mask, board); print_board(board, *mask); break;
+        }
     case 4:
         {
             cout << "Choisissez la piece (x,y) : ";
@@ -351,7 +351,7 @@ void sous_mask_choices(int choix, type_board board, type_mask* mask)
 
 void mask_choices(type_board board, type_mask mask)
 {
-    int choix = 0, x, y;
+    int choix = 0;
     choix = sous_mask_choices(choix);
     string end;
     while(end != "Non")
