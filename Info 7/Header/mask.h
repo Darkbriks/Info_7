b88cf5fb6506c4cc6feb9575020482377d78ee78
Zyre28 <116887859+Zyre28@users.noreply.github.com>
2023-05-10 13:57:48 +0100

    Update mask.h

--- Info 7/Header/mask.h
+++ Info 7/Header/mask.h
@@ -1,4 +1,4 @@
-﻿#ifndef MASK
+#ifndef MASK
 #define MASK
 
 #include "type.h"
@@ -15,5 +15,9 @@ void highlight_possible_moves_bishop(int x, int y, type_mask *mask, type_board b
 void highlight_possible_moves_queen(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a queen (all squares in all directions)
 void highlight_possible_moves_knight(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a knight (L shape)
 void highlight_possible_moves_pawn(int x, int y, type_mask *mask, type_board board); // Highlights all possible moves for a pawn (one square forward, two squares forward (if it's the first move), and one square diagonally forward (if there's an enemy piece))
+int sous_mask_choices(int choix);
+void sous_mask_choices(int choix , type_board board, type_mask* mask);
+void mask_choices_menu(type_board board, type_mask mask);
+void mask_choices(type_board board, type_mask mask);
 
 #endif
