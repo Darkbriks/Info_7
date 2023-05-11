94fb474b924d46c1394008174cc763697b087003
Zyre28 <116887859+Zyre28@users.noreply.github.com>
2023-05-11 19:09:25 +0100

    human move

--- Info 7/Header/game.h
+++ Info 7/Header/game.h
@@ -3,10 +3,14 @@
 
 #include "type.h"
 
-void one_run(int Joueur);
+//void one_run(int Joueur);
 
-void one_run_human();
-void choose_mouvement_human();
+//void one_run_human();
+void one_run(char Joueur, type_board* board, type_mask* mask);
+void one_run_human(type_board* board);
+void one_run_computer(type_board* board, type_mask* mask);
+void choose_mouvement_human(type_board* board, type_mask* mask);
+bool test_run(int lig, int col, int f_lig, int f_col, type_board board, type_mask mask);
 
 void one_run_computer();
 void choose_mouvement_computer();
