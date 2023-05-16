c68ad0b6a3933cc304398ea1693b0971e6d4e0ca
Zyre28 <116887859+Zyre28@users.noreply.github.com>
2023-05-16 02:22:22 +0100

    Add files via upload

---
+++ Info 7/Source/historique.cpp
@@ -0,0 +1,29 @@
+﻿#include "../Header/historique.h"
+#include <iostream>
+#include <string>
+#include "../Header/board.h"
+#include "../Header/type.h"
+#include "../Header/view.h"
+
+/**
+ * @brief Print board with each move one by one
+ * \n WARNING : not tested
+ * @param history 
+ */
+
+void play_historique(type_history history)
+{
+    
+    type_board board;
+    type_mask mask;
+    print_board(board, mask);
+    system("pause");
+    while(history.move_history != NULL and history.move_history != NULL)
+    {
+        move_piece(std::stoi(history.move_history[0].get_val()),std::stoi(history.move_history[1].get_val()),std::stoi(history.move_history[2].get_val()),std::stoi(history.move_history[3].get_val()),board);
+        print_board(board,mask);
+        system("pause");
+        history.board_history = history.board_history->next;
+        history.move_history = history.move_history->next;
+    }
+}
