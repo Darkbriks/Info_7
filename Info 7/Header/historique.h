4fa9999facafe9096d1c98ee701a44a519f515e9
Zyre28 <116887859+Zyre28@users.noreply.github.com>
2023-05-16 02:21:29 +0100

    histo

---
+++ Info 7/Header/historique.h
@@ -0,0 +1,8 @@
+﻿#ifndef HISTORY
+#define HISTORY
+#include "type.h"
+//fonction non necessaire car directictement fait dans les fonctions 
+void play_historique(type_history history); // joue l'historique donné
+
+#endif
+
